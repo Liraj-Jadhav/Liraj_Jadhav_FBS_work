b89342a573cdde8b8e08e29b82a2bec653ee1fd7
Liraj Jadhav <lirajjadhav1164@gmail.com>
2025-07-01 13:46:49 +0530

    Add files via upload

---
+++ Core Python/First test of python/3.km_to_metere_centimeter.py
@@ -0,0 +1,14 @@
+# 3. km to meter and centimeter
+
+# Take input from the user
+kilometer = float(input("Enter distance in kilometers: "))
+
+# Convert into meters
+meter = kilometer * 1000
+
+# Convert into centimeters
+centimeter = kilometer * 100000
+
+# Display the results
+print("Distance  of km into meters:", meter,"m")
+print("Distance of km  into centimeters:", centimeter,"cm")
