f0d8f135818d71562adc4f9a9f74b6d4feee6004
Liraj Jadhav <lirajjadhav1164@gmail.com>
2025-07-02 23:11:55 +0530

    Delete Core Python/Core Python Assignment 1 directory

--- Core Python/Core Python Assignment 1/2.rectanglearea.py
+++
@@ -1,9 +0,0 @@
-
-# 2. Write a program to calculate area of rectangle based on length and breadth.
-
-l= float(input("Enter the length of the rectangle: "))
-b = float(input("Enter the breadth of the rectangle: "))
-
-area = l * b
-
-print("Area of the rectangle = ",area)
