21db0e7a665ba26e46785a961cdc330d54753a4f
Liraj Jadhav <lirajjadhav1164@gmail.com>
2025-07-03 00:30:01 +0530

    Add files via upload

---
+++ Test of  Core python/1st test of python/1.areaperimeter_Figure.py
@@ -0,0 +1,21 @@
+l= float(input("Enter the length of rectangle: "))
+b= float(input("Enter the breadth of rectangle: "))
+r = float(input("Enter the radius of half circle: "))
+
+# Calculate area and perimeter of rectangle
+rect_area = l * b
+rect_perimeter = 2 * (l + b)
+
+#  Calculate area and perimeter of halfcircle
+pi = 3.1416
+halfcircle_area =( pi * r* r)/2
+halfcircle_perimeter = (2 * pi * r)/2
+
+#Calculate the area  and perimeter of the figure
+
+totalarea_figure= rect_area + halfcircle_area
+
+totalperimeter_figure= rect_perimeter + halfcircle_perimeter
+
+print("total area of the figure = ", totalarea_figure)
+print("totalperimeter of the figure = ", totalperimeter_figure)
